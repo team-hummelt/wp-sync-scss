959130db594219a71e7cd88130aa662ea87c2d41
Jens Wiecker <jens@harz-web.com>
2024-11-27 11:55:22 +0100

    add react

---
+++ admin/react/webpack.config.js
@@ -0,0 +1,78 @@
+const path = require("path");
+const ReactRefreshWebpackPlugin = require("@pmmmwh/react-refresh-webpack-plugin");
+const MiniCssExtractPlugin = require("mini-css-extract-plugin");
+const HtmlWebpackPlugin = require("html-webpack-plugin");
+const {CleanWebpackPlugin} = require("clean-webpack-plugin");
+
+
+let mode = "development";
+let target = "web";
+
+const plugins = [
+    new CleanWebpackPlugin(),
+    new MiniCssExtractPlugin(),
+
+   /* new HtmlWebpackPlugin({
+        template: "./src/index.html",
+    }),*/
+];
+
+if (process.env.NODE_ENV === "production") {
+    mode = "production";
+    target = "browserslist";
+}
+if (process.env.SERVE) {
+    // We only want React Hot Reloading in serve mode
+    plugins.push(new ReactRefreshWebpackPlugin());
+}
+
+
+module.exports = {
+    mode: mode,
+    target: target,
+    entry: "./src/index.js",
+    output: {
+        path: path.resolve(__dirname, "dist"),
+        assetModuleFilename: "images/[hash][ext][query]"
+    },
+    module: {
+        rules: [
+            {
+                test: /\.(png|jpe?g|gif|svg)$/i,
+                type: "asset",
+            },
+            {
+                test: /\.(s[ac]|c)ss$/i,
+                use: [
+                    {
+                        loader: MiniCssExtractPlugin.loader,
+                        options: {publicPath: ""},
+                    },
+                    "css-loader",
+                    "postcss-loader",
+                    "sass-loader"
+                ],
+            },
+            {
+                test: /\.jsx?$/,
+                exclude: /node_module/,
+                use: {
+                    loader: "babel-loader"
+                }
+            }
+        ],
+    },
+    plugins: plugins,
+    resolve: {
+        extensions: [".js", ".jsx"]
+    },
+    //devtool: "source-map",
+    devtool: false,
+    devServer: {
+        static: {
+            directory:"./dist",
+        },
+        //contentBase: "./dist",
+        hot: true,
+    }
+}
